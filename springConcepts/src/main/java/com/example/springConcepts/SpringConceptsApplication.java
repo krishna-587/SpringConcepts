9fd53daf51defbcaf48d588c195075be41c35d5b
Krishna gopal singh <krishnagopalsingh587@gmail.com>
2025-03-01 23:29:06 +0530

    Added UC2-Add Logger

--- springConcepts/src/main/java/com/example/springConcepts/SpringConceptsApplication.java
+++ springConcepts/src/main/java/com/example/springConcepts/SpringConceptsApplication.java
@@ -1,19 +1,23 @@
 package com.example.springConcepts;
-
-import com.example.springConcepts.components.DemoBean;
-
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.example.springConcepts.components.DemoBean;
+
+
+
+
 @SpringBootApplication
 public class SpringConceptsApplication {
-
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concept Demo");
-		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
-		System.out.println("Checking Context: "+ context.getBean(DemoBean.class));
+		logger.debug("Welcome to Spring Concept Demo");
+		ApplicationContext context  = SpringApplication.run(SpringConceptsApplication.class, args);
+		logger.debug("Checking Context: ",context.getBean(DemoBean.class));
+		logger.debug("\n*** Example Using @Autowire annotation on property ***");
 	}
 
 }
